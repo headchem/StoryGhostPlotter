3b160822525a6935dfc371ec3b16043a8ed0c76f
John Parsons <jparsons@rand.org>
2021-12-13 19:22:19 -0800

    added custom function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/HttpTrigger1`)).json();
+      console.log('text was: ' + text);
+      setData('testing...' + text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
