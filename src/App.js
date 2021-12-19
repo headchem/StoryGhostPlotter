535715b0951ee47ca326767191c5bbe5390e2c11
John Parsons <jparsons@rand.org>
2021-12-18 22:07:37 -0800

    removed unused var that caused build/deploy to fail?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+//import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
