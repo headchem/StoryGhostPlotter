84fb820dbd4b68ff76a8f320af890632410c0dd9
John Parsons <jparsons@rand.org>
2022-04-05 15:03:17 -0700

    update npm package-lock version

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,6 @@
 const About = () => {
     return (
         <div>
-            <p>About page content here...</p>
             <h4>Version 1.0.0</h4>
             {/* <Link to="/write">Write</Link> */}
         </div>
