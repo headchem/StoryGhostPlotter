2153e6b3a9ac876c5bf4c84ba4b7c8acd2d9b450
John Parsons <jparsons@rand.org>
2022-08-23 21:27:16 -0700

    add more emotions

---
+++ api/models/emotions/Nostalgia.cs
@@ -0,0 +1,33 @@
+using StoryGhost.Interfaces;
+using StoryGhost.Enums;
+using System.Collections.Generic;
+
+namespace StoryGhost.Models.Emotions;
+
+public class Nostalgia : IEmotion
+{
+    public string Id { get { return "nostalgia"; } }
+    public string Name { get { return "Nostalgia"; } }
+    public string Description { get { return ""; } }
+    public List<string> Synonyms { get { return new List<string> { }; } }
+
+    public List<string> Kinds { get { return new List<string> { EmotionKindEnum.EventRelated }; } }
+
+    public double JoyToSadness { get { return .3; } }
+    public double TrustToDisgust { get { return -.2; } }
+    public double FearToAnger { get { return 0; } }
+    public double SurpriseToAnticipation { get { return 0; } }
+
+    public double AnxietyToConfidence { get { return .1111111; } }
+    public double BoredomToFascination { get { return .1111111; } }
+    public double FrustrationToEuphoria { get { return .1111111; } }
+    public double DispiritedToEncouraged { get { return .1111111; } }
+    public double TerrorToEnchantment { get { return .1111111; } }
+    public double HumiliationToPride { get { return .1111111; } }
+
+    public double PleasureToDispleasure { get { return .12; } }
+    public double ArousalToNonarousal { get { return .31; } }
+    public double DominanceToSubmissiveness { get { return .48; } }
+
+    public double InnerFocusToOutwardTarget { get { return -.8; } }
+}
---
+++ api/models/emotions/Pleasure.cs
@@ -0,0 +1,33 @@
+using StoryGhost.Interfaces;
+using StoryGhost.Enums;
+using System.Collections.Generic;
+
+namespace StoryGhost.Models.Emotions;
+
+public class Pleasure : IEmotion
+{
+    public string Id { get { return "pleasure"; } }
+    public string Name { get { return "Pleasure"; } }
+    public string Description { get { return ""; } }
+    public List<string> Synonyms { get { return new List<string> { }; } }
+
+    public List<string> Kinds { get { return new List<string> { EmotionKindEnum.EventRelated }; } }
+
+    public double JoyToSadness { get { return -.7; } }
+    public double TrustToDisgust { get { return -.3; } }
+    public double FearToAnger { get { return 0; } }
+    public double SurpriseToAnticipation { get { return .4; } }
+
+    public double AnxietyToConfidence { get { return .1111111; } }
+    public double BoredomToFascination { get { return .1111111; } }
+    public double FrustrationToEuphoria { get { return .1111111; } }
+    public double DispiritedToEncouraged { get { return .1111111; } }
+    public double TerrorToEnchantment { get { return .1111111; } }
+    public double HumiliationToPride { get { return .1111111; } }
+
+    public double PleasureToDispleasure { get { return -.86; } }
+    public double ArousalToNonarousal { get { return -.28; } }
+    public double DominanceToSubmissiveness { get { return -.37; } }
+
+    public double InnerFocusToOutwardTarget { get { return -.5; } }
+}
