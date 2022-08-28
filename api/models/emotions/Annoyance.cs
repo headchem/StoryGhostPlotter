7826e4e52f8e5096ea4fa9bbb924932df5fb8ce0
John Parsons <jparsons@rand.org>
2022-08-27 21:16:50 -0700

    add more MIT emotion axes

--- api/models/emotions/Annoyance.cs
+++ api/models/emotions/Annoyance.cs
@@ -18,12 +18,12 @@ public class Annoyance : IEmotion
     public double FearToAnger { get { return .33; } }
     public double SurpriseToAnticipation { get { return 0; } }
 
-    public double AnxietyToConfidence { get { return .1111111; } }
-    public double BoredomToFascination { get { return .1111111; } }
-    public double FrustrationToEuphoria { get { return .1111111; } }
-    public double DispiritedToEncouraged { get { return .1111111; } }
-    public double TerrorToEnchantment { get { return .1111111; } }
-    public double HumiliationToPride { get { return .1111111; } }
+    public double AnxietyToConfidence { get { return -.1; } }
+    public double BoredomToFascination { get { return -.1; } }
+    public double FrustrationToEuphoria { get { return -.4; } }
+    public double DispiritedToEncouraged { get { return -.2; } }
+    public double TerrorToEnchantment { get { return 0; } }
+    public double HumiliationToPride { get { return 0; } }
 
     public double PleasureToDispleasure { get { return 0.72; } }
     public double ArousalToNonarousal { get { return -0.49; } }
