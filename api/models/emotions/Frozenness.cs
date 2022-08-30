021410093c35649433c8727e46d117ee1dbe3ddd
John Parsons <jparsons@rand.org>
2022-08-29 23:07:29 -0700

    add more MIT emotion axes

--- api/models/emotions/Frozenness.cs
+++ api/models/emotions/Frozenness.cs
@@ -18,12 +18,12 @@ public class Frozenness : IEmotion
     public double FearToAnger { get { return 0; } } // frozeness = fear + anger
     public double SurpriseToAnticipation { get { return -.7; } }
 
-    public double AnxietyToConfidence { get { return .1111111; } }
-    public double BoredomToFascination { get { return .1111111; } }
-    public double FrustrationToEuphoria { get { return .1111111; } }
-    public double DispiritedToEncouraged { get { return .1111111; } }
-    public double TerrorToEnchantment { get { return .1111111; } }
-    public double HumiliationToPride { get { return .1111111; } }
+    public double AnxietyToConfidence { get { return -.7; } }
+    public double BoredomToFascination { get { return .3; } }
+    public double FrustrationToEuphoria { get { return -1.0; } }
+    public double DispiritedToEncouraged { get { return -.5; } }
+    public double TerrorToEnchantment { get { return -.1; } }
+    public double HumiliationToPride { get { return -.3; } }
 
     public double PleasureToDispleasure { get { return .54; } }
     public double ArousalToNonarousal { get { return .19; } }
