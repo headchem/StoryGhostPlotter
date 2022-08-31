af72224e4da366a43c0d7842e184c5a8a74bcd19
John Parsons <jparsons@rand.org>
2022-08-30 21:58:47 -0700

    add more MIT emotion axes

--- api/models/emotions/Revulsion.cs
+++ api/models/emotions/Revulsion.cs
@@ -18,12 +18,12 @@ public class Revulsion : IEmotion
     public double FearToAnger { get { return .8; } }
     public double SurpriseToAnticipation { get { return -.8; } }
 
-    public double AnxietyToConfidence { get { return .1111111; } }
-    public double BoredomToFascination { get { return .1111111; } }
-    public double FrustrationToEuphoria { get { return .1111111; } }
-    public double DispiritedToEncouraged { get { return .1111111; } }
-    public double TerrorToEnchantment { get { return .1111111; } }
-    public double HumiliationToPride { get { return .1111111; } }
+    public double AnxietyToConfidence { get { return -.5; } }
+    public double BoredomToFascination { get { return .5; } }
+    public double FrustrationToEuphoria { get { return -1.0; } }
+    public double DispiritedToEncouraged { get { return -.5; } }
+    public double TerrorToEnchantment { get { return -.5; } }
+    public double HumiliationToPride { get { return 0; } }
 
     public double PleasureToDispleasure { get { return .79; } }
     public double ArousalToNonarousal { get { return -.53; } }
