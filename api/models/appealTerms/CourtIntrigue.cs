0d7f4a44fe9b8dd072b223be08512cebdd67a571
John David Parsons <jdparsons.dev@gmail.com>
2022-09-27 10:11:40 -0700

    add history genre to appeal terms

--- api/models/appealTerms/CourtIntrigue.cs
+++ api/models/appealTerms/CourtIntrigue.cs
@@ -10,7 +10,7 @@ public class CourtIntrigue : IAppealTerm
     public string Id { get { return "CourtIntrigue"; } }
     public string Name { get { return "Court intrigue"; } }
     public string Description { get { return "What's better than power? More power."; } }
-    public List<string> Genres { get { return new List<string> { "fantasy" }; } }
+    public List<string> Genres { get { return new List<string> { "fantasy", "history" }; } }
     public List<string> Types { get { return new List<string> { "Plot" }; } }
     public List<string> Aliases { get { return new List<string> { }; } }
     public string GetExampleLogLine(List<string> eras, List<string> locations, List<string> keywords)
--- api/models/appealTerms/RightfulHeir.cs
+++ api/models/appealTerms/RightfulHeir.cs
@@ -10,7 +10,7 @@ public class RightfulHeir : IAppealTerm
     public string Id { get { return "RightfulHeir"; } }
     public string Name { get { return "Rightful heir"; } }
     public string Description { get { return "Usurpers and traitors stand in the way of the throne."; } }
-    public List<string> Genres { get { return new List<string> { "fantasy" }; } }
+    public List<string> Genres { get { return new List<string> { "fantasy", "history" }; } }
     public List<string> Types { get { return new List<string> { "Characters" }; } }
     public List<string> Aliases { get { return new List<string> { }; } }
     public string GetExampleLogLine(List<string> eras, List<string> locations, List<string> keywords)
