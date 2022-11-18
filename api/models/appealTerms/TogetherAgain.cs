bfbd7f3cfb6579d95ed470764140b2caca3baef8
John David Parsons <jdparsons.dev@gmail.com>
2022-11-17 22:54:09 -0800

    update appeal terms descriptions

--- api/models/appealTerms/TogetherAgain.cs
+++ api/models/appealTerms/TogetherAgain.cs
@@ -11,7 +11,7 @@ public class TogetherAgain : IAppealTerm
 {
     public string Id { get { return "TogetherAgain"; } }
     public string Name { get { return "Together again"; } }
-    public string Description { get { return "Reunited and it feels so good!"; } }
+    public string Description { get { return "Characters reunite after a long absence, but can they pick back up where they left off?"; } }
     public List<string> Genres { get { return new List<string> { GenresEnum.Romance }; } }
     public List<string> Categories { get { return new List<string> { AppealTermsCategoryEnum.Characters }; } }
     public List<string> Aliases { get { return new List<string> { }; } }
